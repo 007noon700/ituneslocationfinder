c6d294843e8436d22397bfb596d2868b2c794d8c
alex <alexchow700@gmail.com>
2018-12-05 02:38:07 -0600

    works as a standalone function, run this locally

--- iTunesTraverse.py
+++ iTunesTraverse.py
@@ -3,19 +3,28 @@ from libpytunes import Library
 from collections import Counter
 import musicbrainzngs
 import requests
-# import boto3
+import boto3
+import pycountry
+import pygal
+import pygal_maps_world
 
 musicbrainzngs.set_useragent("Artist Nationality Sorter", "a0.0.2", "alexchow.me")
 
 
-l = Library('/Users/alexchow/Music/iTunes/iTunes Music Library.xml')
-# l= Library('iTunes Music Library.xml')
+# l = Library('/Users/alexchow/Music/iTunes/iTunes Music Library.xml')
+l= Library('iTunes Music Library.xml')
 artists = []
 xmls = []
 countries = []
 cities = []
 
 def findArtist():
+    # get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
+    # s3 = boto3.client('s3')
+    # objs = s3.list_objects_v2(Bucket='itunesartistlocationmapper')['Contents']
+    # last_added = [obj['Key'] for obj in sorted(objs, key=get_last_modified)][0]
+    # print(last_added)
+    # l=Library(last_added)
     for id, song in l.songs.items():
         if song.artist is None:
             continue
@@ -66,6 +75,29 @@ def findArtist():
             countries.append(item['artist-list'][0]['area']['name'])
     print(countries)
     totalCount = Counter(countries)
-    print(totalCount)
+    return totalCount
+
+def mapper():
+    countrycodes = []
+    for country in countries:
+        if country is not 'Unknown':
+            country = pycountry.countries.get(name=country)
+        else:
+            continue
+        countrycodes.append(country.alpha_2.lower())
+    print(countrycodes)
+    totalCount = Counter(countrycodes)
+    return totalCount
+
+class tester:
+    findArtist()
+    count=dict(mapper())
+    print(count)
+    worldmap_chart = pygal.maps.world.World()
+    worldmap_chart.title = 'Concentration of Artists in your iTunes Library'
+    worldmap_chart.add('Number of Artists', count)
+    worldmap_chart.render_to_file('chart.svg')
+
+
 
 
